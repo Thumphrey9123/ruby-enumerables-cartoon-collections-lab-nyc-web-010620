985effb6d633b111ded3d21157fce930ea3453c3
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-31 04:28:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,10 @@
-def roll_call_dwarves (arr)
+def roll_call_dwarves(arr)
 arr.each {|x| p x}
 end
-
-roll_call_dwarves(dwarves)
 
 def summon_captain_planet(arr)
 arr.collect {|x| "#{x.capitalize}!"}
 end
-
-puts summon_captain_planet(planeteer_calls)
-
 
 def long_planeteer_calls(arr)
 arr.any? {|x| x.length > 4}
