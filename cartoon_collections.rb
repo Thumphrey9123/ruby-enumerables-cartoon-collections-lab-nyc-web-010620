3bfb74885b7ccda72fb73e2827c783daeaf77605
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-31 04:54:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,14 @@ arr.any? {|x| x.length > 4}
 end
 
 def find_the_cheese(arr)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  (arr & cheese_types).any?
+cheese_types = ["cheddar", "gouda", "camembert"]
+#(arr & cheese_types).any?
+i=0
+while i<cheese_types.length do
+if arr.include?(cheese_types[i])
+return cheese_types[i]
+end
+i+=1
+end
+nil
 end
